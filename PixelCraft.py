3b63f2ed971385cf46c88464b38fd6949a16f9a2
kassumassu <142390041+kassumassu@users.noreply.github.com>
2024-04-12 21:45:07 +0300

    Update PixelCraft.py

--- PixelCraft.py
+++ PixelCraft.py
@@ -1,52 +1,56 @@
 from ursina import * 
 from ursina.prefabs.first_person_controller import * 
-from ursina.prefabs.health_bar import * 
 import random 
-from perlin_noise import PerlinNoise 
-noise = PerlinNoise(octaves=3, seed=random.randint(1, 1000)) 
+from perlin_noise import PerlinNoise
+noise = PerlinNoise(octaves=3, seed=random.randint(1, 1000))
 app = Ursina()
 window.fullscreen=True 
 Sky(texture="sky_sunset") 
-player=FirstPersonController(collider='box', speed=10, y=100) 
-class Voxel(Button): 
-    def __init__(self, position=(0,0,0)): 
+selected_block = "grass"
+player = FirstPersonController(collider='box', speed=10, mouse_sensitivity=Vec2(100,100),
+                               position=(0,100,0)) 
+block_textures = {
+    "grass" : load_texture("grass"),  
+    "brick" : load_texture("brick")
+} 
+class Block(Entity): 
+    def __init__(self, position, block_type): 
         super().__init__(
-            model="cube",
-            parent=scene, 
-            color=color.green, 
-            texture="grass", 
-            collider="box", 
             position=position, 
-            highlight_color=color.lime
-        ) 
-    def input(self, key): 
-        if self.hovered: 
-            if key=="t": 
-                arm.position=(0.6, -0.5)
-                voxel = Voxel(self.position + mouse.normal) 
-            elif key=="y": 
-                arm.position=(0.6, -0.5)
-                destroy(self) 
-            else: 
-                arm.position=(0.75, -0.4)
-        if key=="o": 
-            player.speed=15 
-        if key=="p": 
-            player.speed=10
-min_height = -4 
+            model="cube", 
+            scale=1, 
+            origin_y=-0.5, 
+            texture=block_textures.get(block_type), 
+            collider='box' 
+        )
+        self.block_type = block_type 
+mini_block = Entity(
+    parent=camera, 
+    model="cube", 
+    scale=0.2, 
+    texture=block_textures.get(selected_block), 
+    position=(0.35, -0.25, 0.5),
+    rotation=(-15, -30, -5)
+)
+min_height = -3
 for x in range(-10, 10): 
     for z in range(-10, 10): 
-        height=noise([x * 0.02, z * 0.02])
-        height=math.floor(height * 7.5)
+        height = noise([x * 0.02, z * 0.02])
+        height = math.floor(height * 7.5) 
         for y in range(height, min_height - 1, -1): 
-            voxel = Voxel(position=(x,y + min_height,z))
-arm=Entity(
-    model="cube", 
-    parent=camera.ui,  
-    scale=(0.2, 0.2, 0.5), 
-    position=(0.75, -0.4), 
-    rotation=(-150, -5, 6), 
-    texture="grass", 
-    color=color.green
-)
+            block = Block((x, y + min_height, z), "grass") 
+def input(key): 
+    global selected_block 
+    if key=="t": 
+        hit_info = raycast(camera.world_position, camera.forward, distance=10) 
+        if hit_info.hit: 
+            block = Block(hit_info.entity.position + hit_info.normal, selected_block)
+    if key=="y" and mouse.hovered_entity: 
+        destroy(mouse.hovered_entity)
+    if key=="1": 
+        selected_block = "grass"
+    if key=="2": 
+        selected_block = "brick" 
+def update(): 
+    mini_block.texture=block_textures.get(selected_block)
 app.run()
